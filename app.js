7874e3355a3ab13244dc4c9f06a3c1a61bb65efc
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2024-05-06 23:30:14 +0300

    update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const usersRouter = require('./routes/api/users');
+const booksRouter = require('./routes/api/books');
+const plansRouter = require('./routes/api/plans');
 
 const app = express();
 
@@ -12,7 +14,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/books', booksRouter);
+app.use('/api/plans', plansRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
